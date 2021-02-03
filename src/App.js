06820295f6cad73477851c5679637ad93c30d84e
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2021-02-03 13:38:36 +0530

    ➕ Add router & URLs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import AboutScreen from './screens/AboutScreen';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Route path="/cart" component={CartScreen} />
+          <Route path="/about" component={AboutScreen} />
+          <Route path="/" component={HomeScreen} exact />
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
