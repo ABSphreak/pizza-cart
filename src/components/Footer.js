7fe8ce83fabd023010dbe4d2032bcc5fd03f027b
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2021-02-03 13:39:05 +0530

    ➕ Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer``;
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <p>Copyright &copy;2021 Pizza Cart. All rights reserved.</p>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
