7b32cc31d246d4465599fee83bbcd19c495892fd
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2021-02-06 22:05:48 +0530

    ➕ Add Actions & Constants

---
+++ src/actions/cartActions.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(
+    `https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68/`
+  );
+
+  const selectedData = await data.filter((d) => (d.id = id));
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: selectedData[0].id,
+      name: selectedData[0].name,
+      img_url: selectedData[0].img_url,
+      price: selectedData[0].price,
+      isVeg: selectedData[0].isVeg,
+      qty,
+    },
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
