72eb9c4935c94d09d7ad84f6ef5afb4b045e2899
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2021-02-07 22:58:39 +0530

    ➕ Add About content

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,5 +1,41 @@
+import styled from 'styled-components';
+
+const AboutWrapper = styled.div`
+  background-color: #f5f3f4;
+  z-index: 1;
+  padding: 0 2em 3em 2em;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .content {
+    .header {
+      font-size: 1.25rem;
+      h1 {
+        padding: 0.1em 0;
+        font-size: 2.5rem;
+        font-weight: 900;
+        letter-spacing: -1.25px;
+        background: -webkit-linear-gradient(to right, #c94b4b, #4b134f);
+        background: linear-gradient(to right, #c94b4b, #4b134f);
+        background-clip: unset;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+  }
+`;
+
 const AboutScreen = () => {
-  return <div>About Screen</div>;
+  return (
+    <AboutWrapper>
+      <div className="content">
+        <div className="header">
+          <p>Created by</p> <h1>Abhinav Sharma</h1> <p>(@ABSphreak)</p>
+        </div>
+      </div>
+    </AboutWrapper>
+  );
 };
 
 export default AboutScreen;
